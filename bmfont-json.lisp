e2cfece742b97cdb938c207ead55292164b4a92b
Bart Botta <00003b@gmail.com>
2022-03-14 05:36:26 -0500

    store value of distance range correctly

--- bmfont-json.lisp
+++ bmfont-json.lisp
@@ -179,7 +179,7 @@
              ("fieldType" (string-downcase
                            (getf (distance-field f) :field-type "")))
              ("distanceRange"
-              (getf (distance-field f) :field-type 0))))))
+              (getf (distance-field f) :distance-range 0))))))
       (format
        stream
        "~a"
